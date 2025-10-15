ca646c0bf067f4e2517c712ac2590fe0324d04ae
mohd. anas <anas9650731343@gmail.com>
2025-10-15 21:16:33 +0530

    Add files via upload

--- pattern10.cpp
+++ pattern10.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
 using namespace std;
- int main(){
-    for(int i=1; i<6; i++){
-        for(int j=0; j<i; j++){
+int hello(int n){
+    for(int i=0; i<2*n-1; i++){
+        int stars =i;
+
+        if(i>n){
+            stars =2*n-i;
+        }
+        for(int j=0; j<=stars; j++){
             cout<<"*";
         }
-       cout<<endl;
- }
-for(int i=4; i>0; i--){
-    for(int j=0; j<i; j++){
-        cout<<"*";
+        cout<<endl;
     }
-    cout<<endl;
 }
+
+int main(){
+int n;
+cout<<"enter the value of n: ";
+cin>>n;
+hello(n);
 }
